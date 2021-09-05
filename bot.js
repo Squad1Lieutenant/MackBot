4e11260800745176dd274cb4c8bf0fca4aee0381
Jack Pirone <jpirone@winchendon.org>
2021-09-05 14:13:01 -0400

    Can now grab the events webpage

---
+++ bot.js
@@ -0,0 +1,2 @@
+const discord = require(discord.js);
+const token = tokens.json()
--- web-grabber.js
+++ web-grabber.js
@@ -1,14 +1,25 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var codeFormatter = require('js-beautify').html;
+const fs = require('fs');
 
 function httpGetAsync(theUrl, callback) {
     let xmlHttpReq = new XMLHttpRequest();
     xmlHttpReq.onreadystatechange = function () {
-      if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200)
+      if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
         callback(xmlHttpReq.responseText);
+      }
     }
-    xmlHttpReq.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttpReq.open("GET", theUrl, true);
     xmlHttpReq.send(null);
   }
-  httpGetAsync('https://www.merrimack.edu/campus-life/getting-involved/campus-programming-calendar.php', function(result){
-      console.log(result);
+  
+  httpGetAsync('https://www.merrimack.edu/campus-life/getting-involved/campus-programming-calendar.php', function(result) {
+      var data = codeFormatter(result);
+      console.log(data);
+      fs.writeFile('~mack.html', data, err => {
+        if (err) {
+          console.error(err);
+          return
+        }
+      })
   });
