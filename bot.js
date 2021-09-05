4fbe0d4b7cfd6fc34e60af3e08b6366e66b3a6e1
Jack Pirone <jpirone@winchendon.org>
2021-09-05 15:34:49 -0400

    Switched to Synchronous file reading to load HTML file

--- bot.js
+++ bot.js
@@ -1,16 +1,21 @@
 const webGrabber = require("./web-grabber");
 const fs = require('fs');
 const jsdom = require("jsdom");
-const jquery = require('jquery')
+const jquery = require('jquery');
+const { error } = require("console");
 const { JSDOM } = jsdom;
 
 
 webGrabber.writeToFile();
 
-fs.readFile('mack.html', {encoding: "utf8"}, function (err, markup) {
-    if (err) throw err;
-    const dom = new JSDOM(markup);
-    const $ = jquery(dom.window);
-    const data = $("div.lw_today").html();
-    console.log(data);
-});
+try {
+    const fileHtml = fs.readFileSync('mack.html');
+    var file = fileHtml;
+} catch(err){
+    console.log(err)
+    return
+}
+const dom = new JSDOM(file);
+const $ = jquery(dom.window);
+const data = $("div.lw_today").html();
+console.log(data);
